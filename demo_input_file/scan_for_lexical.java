21838c1d68d1329922f9d88db3ddde7abb8663b1
Neopentene <82939252+Neopentene@users.noreply.github.com>
2022-01-19 12:24:19 +0530

    Update scan_for_lexical.java

--- demo_input_file/scan_for_lexical.java
+++ demo_input_file/scan_for_lexical.java
@@ -1,1 +1,12 @@
-
+class lexical_analysis {
+    public static void main(String[] args) {
+        int a = 2 + 3; // operator test
+        char test = '\0'; //character test
+        System.out.println("Hello World and the value of a is" + a + ' ' + 'o' + 'k'); //This is a comment, char and string test
+        boolean bool = (2 >>> 1 == 3); // bool and lengthy operator test
+        /*
+            More comments to see if it works
+        */ /*Even more comments *//* More Comments ? */
+        // Again
+    }
+}
